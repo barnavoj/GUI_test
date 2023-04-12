2a1445d69c2cf2e11433411b7f1ab29cd73f5505
lolik444 <vojtech.barnat@gmail.com>
2023-04-12 16:11:20 +0200

    ball moving

--- hello_kivy.py
+++ hello_kivy.py
@@ -1,18 +1,12 @@
 from kivy.uix.widget import Widget
 from kivy.app import App
-from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
 from kivy.vector import Vector
+from kivy.clock import Clock
 import kivy
+from random import randint
+
 kivy.require('2.1.0')  # replace with your current kivy version !
-
-
-class PongGame(Widget):
-    pass
-
-
-class PongApp(App):
-    def build(self):
-        return PongGame()
 
 
 class PongBall(Widget):
@@ -28,7 +22,38 @@ class PongBall(Widget):
     # ``move`` function will move the ball one step. This
     #  will be called in equal intervals to animate the ball
     def move(self):
-        self.pos = kivy.vector.Vector(*self.velocity) + self.pos
+        self.pos = Vector(self.pos) + Vector(self.velocity)
+
+
+class PongGame(Widget):
+
+    ball = ObjectProperty(None)
+
+    def serve_ball(self):
+        self.ball.center = self.center
+        self.ball.velocity = Vector(4, 0).rotate(randint(0, 360))
+
+    def update(self, dt):
+        self.ball.move()
+
+        # bounce off top and bottom
+        if (self.ball.y < 0) or (self.ball.top > self.height):
+            self.ball.velocity_y *= -1
+
+        # bounce off left and right
+        if (self.ball.x < 0) or (self.ball.right > self.width):
+            self.ball.velocity_x *= -1
+
+        pass
+
+
+class PongApp(App):
+
+    def build(self):
+        game = PongGame()
+        game.serve_ball()
+        Clock.schedule_interval(game.update, 1.0/60.0)
+        return game
 
 
 if __name__ == '__main__':
